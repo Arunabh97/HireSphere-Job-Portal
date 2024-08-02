79285aa61ba344141ab6b7fb3da31c7eb42e2419
#Grind Microservices <dasarunabh036@gmail.com>
2024-08-02 11:39:20 +0530

    Validate the email no duplicate email

--- src/main/java/com/spring/jobportal/entity/Users.java
+++ src/main/java/com/spring/jobportal/entity/Users.java
@@ -12,7 +12,7 @@ import jakarta.validation.constraints.NotEmpty;
 public class Users {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
 	
 	@Column(unique = true)
--- src/main/java/com/spring/jobportal/repository/UsersRepository.java
+++ src/main/java/com/spring/jobportal/repository/UsersRepository.java
@@ -2,7 +2,10 @@ package com.spring.jobportal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.spring.jobportal.entity.Users;
+import java.util.Optional;
+
 
 public interface UsersRepository extends JpaRepository<Users, Integer>{
 
+	Optional<Users> findByEmail(String email);
 }
