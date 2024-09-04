0861601c7635ebe27b577ac8e30ad778e8376b8c
#Grind Microservices <dasarunabh036@gmail.com>
2024-09-04 09:18:42 +0530

    Added a feature Display and Retrive Jobs

--- src/main/java/com/spring/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/spring/jobportal/controller/JobPostActivityController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,6 +38,10 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
--- src/main/java/com/spring/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/spring/jobportal/services/JobPostActivityService.java
@@ -1,9 +1,16 @@
 package com.spring.jobportal.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.jobportal.entity.IRecruiterJobs;
+import com.spring.jobportal.entity.JobCompany;
+import com.spring.jobportal.entity.JobLocation;
 import com.spring.jobportal.entity.JobPostActivity;
+import com.spring.jobportal.entity.RecruiterJobsDto;
 import com.spring.jobportal.repository.JobPostActivityRepository;
 
 @Service
@@ -11,12 +18,27 @@ public class JobPostActivityService {
 
     private final JobPostActivityRepository jobPostActivityRepository;
 
-    @Autowired
     public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
         this.jobPostActivityRepository = jobPostActivityRepository;
     }
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for (IRecruiterJobs rec : recruiterJobsDtos) {
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(),
+                    rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
+
     }
 }
